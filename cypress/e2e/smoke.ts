09ea4e57ddb95ec947a1e8a96707d51051ae0134
pqmcgill <pqmcgill@gmail.com>
2022-04-12 10:01:19 -0400

    fix e2e tests

--- cypress/e2e/smoke.ts
+++ cypress/e2e/smoke.ts
@@ -1,4 +1,5 @@
 import faker from "@faker-js/faker";
+import type { Spend } from "@prisma/client";
 
 describe("smoke tests", () => {
   afterEach(() => {
@@ -19,30 +20,30 @@ describe("smoke tests", () => {
     cy.findByLabelText(/password/i).type(loginForm.password);
     cy.findByRole("button", { name: /create account/i }).click();
 
-    cy.findByRole("link", { name: /notes/i }).click();
+    cy.findByRole("link", { name: /spends/i }).click();
     cy.findByRole("button", { name: /logout/i }).click();
     cy.findByRole("link", { name: /log in/i });
   });
 
-  it("should allow you to make a note", () => {
-    const testNote = {
-      title: faker.lorem.words(1),
-      body: faker.lorem.sentences(1),
+  it("should allow you to make a spend", () => {
+    const testSpend: Pick<Spend, "amount" | "memo"> = {
+      memo: faker.lorem.words(1),
+      amount: 10.0,
     };
     cy.login();
     cy.visit("/");
 
-    cy.findByRole("link", { name: /notes/i }).click();
-    cy.findByText("No notes yet");
+    cy.findByRole("link", { name: /spends/i }).click();
+    cy.findByText("No spends yet");
 
-    cy.findByRole("link", { name: /\+ new note/i }).click();
+    cy.findByRole("link", { name: /\+ new spend/i }).click();
 
-    cy.findByRole("textbox", { name: /title/i }).type(testNote.title);
-    cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
+    cy.findByRole("textbox", { name: /memo/i }).type(testSpend.memo);
+    // cy.findByRole("textbox", { name: /body/i }).type(testNote.body);
     cy.findByRole("button", { name: /save/i }).click();
 
     cy.findByRole("button", { name: /delete/i }).click();
 
-    cy.findByText("No notes yet");
+    cy.findByText("No spends yet");
   });
 });
